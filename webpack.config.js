f5f47d8f58669967e552cda13946e6752d899278
Kitketovsky <kitketovsky@gmail.com>
2023-04-01 17:55:05 +0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devMode = process.env.NODE_ENV !== 'production';
+
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "./js/main.js",
+        assetModuleFilename: 'assets/images/[name].[ext]',
+        clean: true
+    },
+    devServer: {
+        static: path.join(__dirname, 'dist'), // path to your built files
+        compress: true,
+        port: 9000, // port number for the server,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.hbs$/,
+                use: 'handlebars-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    'postcss-loader',
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.js$/i,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
+                type: 'asset/resource'
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.hbs',
+            title: "Custom title",
+            filename: 'index.html',
+            inject: true,
+            minify: {
+                removeAttributeQuotes: true,
+                collapseWhitespace: true,
+                removeComments: true,
+            },
+        }),
+        new MiniCssExtractPlugin({
+            filename: './css/[name].css'
+        })
+    ]
+}
