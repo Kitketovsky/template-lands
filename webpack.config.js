66f51388583a3eef478b4b5a7cd0c69e0c5d4720
Kitketovsky <kitketovsky@gmail.com>
2023-04-05 16:52:46 +0400

    fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,86 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const devMode = process.env.NODE_ENV !== "production";
-
 const path = require("path");
 
-module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "./js/main.js",
-        assetModuleFilename: "assets/images/[name].[ext]",
-        clean: true,
-    },
-    devServer: {
-        static: path.join(__dirname, "dist"), // path to your built files
-        compress: true,
-        port: 9000, // port number for the server,
-        open: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.hbs$/,
-                loader: "handlebars-loader",
-                options: {
-                    inlineRequires: "assets",
+module.exports = function(env, argv) {
+    const mode = env["production"] ? "production" : "development";
+    const devMode = mode === "development";
+
+    console.log(`Mode: ${mode}`);
+
+    return {
+        mode,
+        entry: ["./src/index.hbs", "./src/index.js"],
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "./js/main.js",
+            clean: true,
+        },
+        devServer: {
+            static: path.join(__dirname, "dist"), // path to your built files
+            compress: true,
+            port: 9000, // port number for the server,
+            open: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.hbs$/,
+                    loader: "handlebars-loader",
+                    options: {
+                        inlineRequires: "assets",
+                    },
+                    exclude: /node_modules/,
                 },
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(sa|sc|c)ss$/i,
-                use: [
-                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-                    "css-loader",
-                    "postcss-loader",
-                    "sass-loader",
-                ],
-            },
-            {
-                test: /\.js$/i,
-                use: ["babel-loader"],
-            },
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff)$/,
-                type: "asset/resource",
-            },
+                {
+                    test: /\.(sa|sc|c)ss$/i,
+                    use: [
+                        devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                        "css-loader",
+                        "postcss-loader",
+                        "sass-loader",
+                    ],
+                },
+                {
+                    test: /\.js$/i,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /\.(png|jpg|gif|svg)$/,
+                    type: "asset/resource",
+                    generator: {
+                        filename: 'assets/images/[name].[ext]'
+                    }
+                },
+                {
+                    test: /\.(eot|ttf|woff|woff2)$/,
+                    type: "asset/resource",
+                    generator: {
+                        filename: 'assets/fonts/[name].[ext]'
+                    }
+                }
+            ],
+        },
+        resolve: {
+            extensions: [".*", ".js"],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.hbs",
+                title: "Custom Title",
+                filename: "index.html",
+                inject: true,
+                // favicon: "./src/public/logo.ico",
+                minify: {
+                    removeAttributeQuotes: true,
+                    collapseWhitespace: true,
+                    removeComments: true,
+                },
+            }),
+            new MiniCssExtractPlugin({
+                filename: "./css/[name].css",
+            }),
         ],
-    },
-    resolve: {
-        extensions: [".*", ".js"],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.hbs",
-            title: "Custom Title",
-            filename: "index.html",
-            inject: true,
-            favicon: "./src/public/logo.ico",
-            minify: {
-                removeAttributeQuotes: true,
-                collapseWhitespace: true,
-                removeComments: true,
-            },
-        }),
-        new MiniCssExtractPlugin({
-            filename: "./css/[name].css",
-        }),
-    ],
+    }
 };
